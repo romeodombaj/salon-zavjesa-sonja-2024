00b5ec4bcebbf02b1b2792ffe78517d988d04ce9
romeodombaj <romeodombaj@gmail.com>
2024-04-04 01:20:00 +0200

    cloudflare optimisations 1

--- app/[portfolioSlug]/page.js
+++ app/[portfolioSlug]/page.js
@@ -1,7 +1,9 @@
+export const runtime = "edge";
+
 import Gallery from "@/components/Portfolio/1_Gallery/Gallery";
 import styles from "./page.module.css";
-import fs from "fs";
-import path from "path";
+//import fs from "fs";
+//import path from "path";
 
 export const getPostLength = (directory) => {
   return fs.readdirSync(directory).length;
@@ -10,10 +12,17 @@ export const getPostLength = (directory) => {
 export default function PortfolioPage({ params }) {
   const slug = params.portfolioSlug;
 
-  const POST_FOLDER_NAME = `images/webImages/${slug}`;
-  const POSTS_DIRECTORY = path.join(process.cwd(), "public", POST_FOLDER_NAME);
+  //const POST_FOLDER_NAME = `images/webImages/${slug}`;
+  //const POSTS_DIRECTORY = path.join(process.cwd(), "public", POST_FOLDER_NAME);
 
-  const length = getPostLength(POSTS_DIRECTORY);
+  //const POSTS_DIRECTORY = `${process.cwd()}/public/images/webImages/${slug}`;
+
+  console.log("HEEEEREEEE");
+  //console.log(POSTS_DIRECTORY);
+
+  //const length = getPostLength(POSTS_DIRECTORY);
+
+  const length = 12;
 
   return (
     <main className={styles.main}>
