b48a3b81312973eb28521f01daf73f6399abce9e
romeodombaj <romeodombaj@gmail.com>
2024-04-09 16:10:06 +0200

    mobile navigation polished

--- components/Navigation/MobileMenu.js
+++ components/Navigation/MobileMenu.js
@@ -1,58 +1,83 @@
 import styles from "./MobileMenu.module.css";
 import Link from "next/link";
+import exitIcon from "@/assets/exit-icon.png";
+import Image from "next/image";
 
-export default function MobileMenu({ pathname }) {
+export default function MobileMenu({ pathname, onMenuCloseHandler }) {
+  const onExit = () => {
+    setTimeout(() => {
+      onMenuCloseHandler();
+    }, 100);
+  };
+
   return (
-    <main className={styles.main}>
-      <Link
-        href="/"
-        className={`${styles.home} ${styles[pathname === "/" && "active"]}`}
-      >
-        NASLOVNA
-      </Link>
-      <div className={styles[`portfolio-list`]}>
-        <Link
-          href="/zavjese"
-          className={`${styles.portfolio} ${
-            styles[pathname === "/zavjese" && "active"]
-          }`}
-        >
-          Zavjese
-        </Link>
-        <Link
-          href="/rolice_paneli"
-          className={`${styles.portfolio} ${
-            styles[pathname === "/rolice_paneli" && "active"]
-          }`}
-        >
-          Rolice i paneli
-        </Link>
-        <Link
-          href="/dekoracija"
-          className={`${styles.portfolio} ${
-            styles[pathname === "/dekoracija" && "active"]
-          }`}
-        >
-          Dekoracija
-        </Link>
-        <Link
-          href="/prateca_dekoracija"
-          className={`${styles.portfolio} ${
-            styles[pathname === "/prateca_dekoracija" && "active"]
-          }`}
-        >
-          Prateća dekoracija
-        </Link>
+    <>
+      <div className={styles.exit}>
+        <div className={styles[`exit-icon`]}>
+          <Image src={exitIcon} alt="Exit." fill onClick={onExit} />
+        </div>
       </div>
+      <main className={styles.main}>
+        <Link
+          onClick={onExit}
+          href="/"
+          className={`${styles.home} ${styles[pathname === "/" && "active"]}`}
+        >
+          NASLOVNA
+        </Link>
 
-      <div className={styles[`info-list`]}>
-        <Link href="/#contact" className={styles.info}>
-          Kontakt
-        </Link>
-        <Link href="/#location" className={styles.info}>
-          Lokacija
-        </Link>
-      </div>
-    </main>
+        <div className={styles.separator} />
+
+        <div className={styles[`portfolio-list`]}>
+          <Link
+            onClick={onExit}
+            href="/zavjese"
+            className={`${styles.portfolio} ${
+              styles[pathname === "/zavjese" && "active"]
+            }`}
+          >
+            Zavjese
+          </Link>
+          <Link
+            onClick={onExit}
+            href="/rolice_paneli"
+            className={`${styles.portfolio} ${
+              styles[pathname === "/rolice_paneli" && "active"]
+            }`}
+          >
+            Rolice i paneli
+          </Link>
+          <Link
+            onClick={onExit}
+            href="/dekoracija"
+            className={`${styles.portfolio} ${
+              styles[pathname === "/dekoracija" && "active"]
+            }`}
+          >
+            Dekoracija
+          </Link>
+          <Link
+            onClick={onExit}
+            href="/prateca_dekoracija"
+            className={`${styles.portfolio} ${
+              styles[pathname === "/prateca_dekoracija" && "active"]
+            }`}
+          >
+            Prateća dekoracija
+          </Link>
+        </div>
+
+        <div className={styles.separator} />
+
+        <div className={styles[`info-list`]}>
+          <Link href="/#contact" className={styles.info} onClick={onExit}>
+            Kontakt
+          </Link>
+          <Link href="/#location" className={styles.info} onClick={onExit}>
+            Lokacija
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -19,7 +19,15 @@ export default function Navbar() {
   const pathname = usePathname();
 
   const [navShrink, setNavShrink] = useState();
-  const [isNavigating, setIsNavigating] = useState(false);
+  const [isNavigating, setIsNavigating] = useState(true);
+
+  const onMenuOpenHandler = () => {
+    !isNavigating && setIsNavigating(true);
+  };
+
+  const onMenuCloseHandler = () => {
+    setIsNavigating(false);
+  };
 
   useEffect(() => {
     if (pathname === "/" && window.scrollY === 0) {
@@ -38,96 +46,100 @@ export default function Navbar() {
   }, [pathname]);
 
   return (
-    <>
-      {isNavigating && <MobileMenu pathname={pathname} />}
-      <div
-        className={`${styles.wrapper} ${styles[navShrink && "wrapper-shrink"]}`}
-      >
-        <div className={`${styles["content-section"]}`}>
+    <div
+      className={`${styles.wrapper} ${styles[navShrink && "wrapper-shrink"]}`}
+    >
+      {isNavigating && (
+        <MobileMenu
+          pathname={pathname}
+          onMenuCloseHandler={onMenuCloseHandler}
+        />
+      )}
+      <div className={`${styles["content-section"]}`}>
+        <Link
+          /*onClick={onNavigate}*/
+          href="/"
+          value=""
+          className={`${styles.home} ${styles[navShrink && "home-shrink"]} ${
+            styles[pathname === "/" && "active"]
+          }`}
+        >
+          NASLOVNA
+        </Link>
+        <div
+          className={`${styles.portfolios} ${
+            styles[navShrink && "portfolios-shrink"]
+          }`}
+        >
+          <PortfolioButton
+            shrink={navShrink}
+            pathname={pathname}
+            value="zavjese"
+          >
+            Zavjese
+          </PortfolioButton>
+          <PortfolioButton
+            pathname={pathname}
+            shrink={navShrink}
+            value="rolice_paneli"
+          >
+            Rolice i paneli
+          </PortfolioButton>
+          <PortfolioButton
+            shrink={navShrink}
+            pathname={pathname}
+            value="dekoracija"
+          >
+            Dekoracija
+          </PortfolioButton>
+          <PortfolioButton
+            pathname={pathname}
+            shrink={navShrink}
+            value="prateca_dekoracija"
+          >
+            Prateća dekoracija
+          </PortfolioButton>
+        </div>
+        <div className={styles.info}>
           <Link
-            /*onClick={onNavigate}*/
-            href="/"
-            value=""
-            className={`${styles.home} ${styles[navShrink && "home-shrink"]} ${
-              styles[pathname === "/" && "active"]
+            href="/#contact"
+            className={`${styles["info-button"]} ${
+              styles[navShrink && ["info-button-shrink"]]
             }`}
           >
-            NASLOVNA
+            <Image
+              src={phoneIcon}
+              alt="Phone."
+              className={styles[`info-icon`]}
+            />
+            <div className={styles[`info-text`]}>099-595-1938</div>
+          </Link>
+          <Link
+            href="/#location"
+            className={`${styles["info-button"]} ${
+              styles[navShrink && ["info-button-shrink"]]
+            }`}
+          >
+            <Image
+              src={locationIcon}
+              alt="Location."
+              className={styles[`info-icon`]}
+            />
           </Link>
           <div
-            className={`${styles.portfolios} ${
-              styles[navShrink && "portfolios-shrink"]
+            className={`${styles["info-button"]} ${styles["hamburger"]} ${
+              styles[navShrink && ["info-button-shrink"]]
             }`}
           >
-            <PortfolioButton
-              shrink={navShrink}
-              pathname={pathname}
-              value="zavjese"
-            >
-              Zavjese
-            </PortfolioButton>
-            <PortfolioButton
-              pathname={pathname}
-              shrink={navShrink}
-              value="rolice_paneli"
-            >
-              Rolice i paneli
-            </PortfolioButton>
-            <PortfolioButton
-              shrink={navShrink}
-              pathname={pathname}
-              value="dekoracija"
-            >
-              Dekoracija
-            </PortfolioButton>
-            <PortfolioButton
-              pathname={pathname}
-              shrink={navShrink}
-              value="prateca_dekoracija"
-            >
-              Prateća dekoracija
-            </PortfolioButton>
-          </div>
-          <div className={styles.info}>
-            <Link
-              href="/#contact"
-              className={`${styles["info-button"]} ${
-                styles[navShrink && ["info-button-shrink"]]
-              }`}
-            >
-              <Image
-                src={phoneIcon}
-                alt="Phone."
-                className={styles[`info-icon`]}
-              />
-              <div className={styles[`info-text`]}>099-595-1938</div>
-            </Link>
-            <Link
-              href="/#location"
-              className={`${styles["info-button"]} ${
-                styles[navShrink && ["info-button-shrink"]]
-              }`}
-            >
-              <Image
-                src={locationIcon}
-                alt="Location."
-                className={styles[`info-icon`]}
-              />
-            </Link>
-            <div
-              className={`${styles["info-button"]} ${styles["hamburger"]} ${
-                styles[navShrink && ["info-button-shrink"]]
-              }`}
-            >
-              <Image
-                src={hamburgerIcon}
-                alt="Menu."
-                className={styles[`info-icon`]}
-              />
-            </div>
+            <Image
+              src={hamburgerIcon}
+              alt="Menu."
+              className={styles[`info-icon`]}
+              onClick={onMenuOpenHandler}
+            />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
